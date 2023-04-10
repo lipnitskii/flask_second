4a2e37b4b7bea0aa91061e0ec867aa89c27f999f
lipnitskii <lmmlipas@gmail.com>
2023-04-10 12:41:42 +0300

    add admin

--- admin/admin.py
+++ admin/admin.py
@@ -1,9 +1,27 @@
-from flask import Blueprint, flash, redirect, render_template, request, session, url_for
+import sqlite3
+from flask import Blueprint, flash, redirect, render_template, request, session, url_for, g
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
 
 menu = [{'url': '.index', 'title': 'Панель'},
-        {'url': '.logout', 'title': 'Выйти'}]
+        {'url': '.logout', 'title': 'Выйти'},
+        {'url': '.listpubs', 'title': 'Список статей'},
+        {'url': '.listusers', 'title': 'Список пользователей'}]
+
+db = None
+
+@admin.before_request
+def before_request():
+    '''Установление связи с бд до выполнения запроса'''
+    global db
+    db = g.get('link_db')
+
+@admin.teardown_request
+def teardown_request(request):
+    global db
+    db = None
+    return request
+
 
 def login_admin():
     session['admin_logged'] = 1
@@ -42,4 +60,34 @@ def logout():
 
     logout_admin()
 
-    return redirect(url_for('.login'))       
+    return redirect(url_for('.login'))   
+
+@admin.route('/list-pubs')
+def listpubs():
+    if not isLogged():
+        return(url_for('.login'))
+
+    list = []
+    try:
+        cur = db.cursor()
+        cur.execute(f'SELECT title, text, url FROM posts')
+        list = cur.fetchall()
+    except sqlite3.Error as e:
+        print('Ошибка получения статей из БД' + str(e))
+
+    return render_template('admin/listpubs.html', title='Список статей', menu=menu, list=list)    
+
+@admin.route('/listusers')
+def listusers():
+    if not isLogged():
+        return(url_for('.login'))
+
+    list = []
+    try:
+        cur = db.cursor()
+        cur.execute(f'SELECT name, email FROM users')
+        list = cur.fetchall()
+    except sqlite3.Error as e:
+        print('Ошибка получения статей из БД' + str(e))
+
+    return render_template('admin/listusers.html', title='Список пользователей', menu=menu, list=list) 
